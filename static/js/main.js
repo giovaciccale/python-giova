af48e2e859bdc9800a2238a6156d2b68ef5dd96b
giovaciccale <65725536+giovaciccale@users.noreply.github.com>
2020-05-21 13:00:21 -0300

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+const btnDelete= document.querySelectorAll('.btn-delete');
+if(btnDelete) {
+  const btnArray = Array.from(btnDelete);
+  btnArray.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      if(!confirm('Estás seguro que se esta resuelto?')){
+        e.preventDefault();
+      }
+    });
+  })
+}
